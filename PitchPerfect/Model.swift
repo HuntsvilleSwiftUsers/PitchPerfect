57c3d74e003ee8a1f234cf0977082218e1fc76aa
Jarrod Parkes <jarrod@udacity.com>
2017-03-16 19:42:14 -0500

    add initial commit

---
+++ PitchPerfect/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  PitchPerfect
+//
+//  Created by Jarrod Parkes on 3/15/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+struct Model {    
+    let name: String
+}
